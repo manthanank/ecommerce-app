38d66e7f114eafd45b84913df984ea33e02210f9
Manthan Ankolekar <manthan.ank@gmail.com>
2022-12-18 00:10:32 +0530

    changes made

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
 const express = require('express')
 const app = express()
-const port = 3000
-const { MongoClient } = require('mongodb');
+const port = process.env.PORT | 3000 
+const mongoose = require("mongoose");
+//const { MongoClient } = require('mongodb');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 require('dotenv').config();
 
+mongoose
+  .connect(
+    "mongodb+srv://manthanank:" +
+      process.env.MONGO_ATLAS_PW +
+      "@cluster0.re3ha3x.mongodb.net/ecommerce-backend-app"
+    // "mongodb+srv://max:QuBqs0T45GDKPlIG@cluster0-ntrwp.mongodb.net/node-angular?retryWrites=true"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+const path = require('path')
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+  console.log(`Server app listening on port ${port} and GraphQL server listening on https://localhost:${port}/graphql`);
 })
